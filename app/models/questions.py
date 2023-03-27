254b2c19697dfb9086d79a427379915732fa2da5
Kristine Dzneladze <qristina.qristina20010203@gmail.com>
2023-03-27 20:19:30 +0400

    added questions and asnwer options tables

---
+++ app/models/questions.py
@@ -0,0 +1,30 @@
+from app.extensions import db
+from app.models.base import BaseModel
+
+
+class Question(BaseModel):
+    __tablename__ = "questions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    question_text = db.Column(db.String, nullable = False )
+    question_description = db.Column(db.String)
+    question_type = db.Column(db.String)
+    min_grade = db.Column(db.Integer)
+    min_grade_text = db.Column(db.String)
+    max_grade = db.Column(db.Integer)
+    max_grade_text = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))    
+    
+    option = db.relationship("QuestionOption", backref="question")
+
+class QuestionOption(BaseModel):
+    __tablename__ = "question_options"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey("questions.id"))
+    text = db.Column(db.String, nullable = False)
+    is_correct  = db.Column(db.Boolean, nullable = False)
+
+
+
+
