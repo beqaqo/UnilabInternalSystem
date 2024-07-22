b4958bbd9df06be82ea8f1327f7035c658ff6632
R-Grigala <r.grigalashvili777@gmail.com>
2024-07-22 16:23:05 +0400

    add Subjects API

--- app/api/nsmodels/subjects.py
+++ app/api/nsmodels/subjects.py
@@ -7,7 +7,7 @@ subjects_ns = api.namespace('Subjects', description='Api endpoint for User Profi
 subjects_parser = reqparse.RequestParser()
 
 subjects_parser.add_argument('name', type=str, required=True, help='Subject name: C++')
-subjects_parser.add_argument('lecturers_id', type=int, action='append', required=True, help='The unique identifier of a lecturer example - 3')
+subjects_parser.add_argument('lecturers_id', type=int, action='append', required=True, help='The unique identifier of a lecturer example: 3')
 subjects_parser.add_argument('course_syllabus', type=str, required=True, help='Example syllabus.pdf')
 subjects_parser.add_argument('internship_syllabus', type=str, required=True, help='Example syllabus.pdf')
 subjects_parser.add_argument('tlt_syllabus', type=str, required=True, help='Example syllabus.pdf')
